04a9361282aed7b5ea9f4a49c0418c1ac79465b1
Christian <chr.vadala@gmail.com>
2021-03-21 00:20:05 +0100

    improves deploy-network script

--- deploy-network.sh
+++ deploy-network.sh
@@ -1,25 +1,24 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 STACK="${STACK:?env not found}"
 BUCKET="${BUCKET:?env not found}"
 REGION="${REGION:-us-east-1}"
 
-echo "STACK=${STACK}"
-echo "REGION=${REGION}"
-echo "BUCKET=${BUCKET}"
-
 TMP_TEMPLATE="/tmp/${STACK}-packed-nested-stacks.json"
-
-set -ex
+S3_TEMPLATE="s3://${BUCKET}/network/network.yml"
+PARAMETERS="file://./${STACK}.json"
 
 aws cloudformation package \
   --template-file "./stacks/network.yaml" \
+  --s3-prefix "network/nested" \
   --output-template "$TMP_TEMPLATE" \
   --s3-bucket "$BUCKET" \
   --region "$REGION"
 
-aws cloudformation deploy \
+ aws s3 cp "$TMP_TEMPLATE" "$S3_TEMPLATE"
+
+ aws cloudformation deploy \
   --template-file "$TMP_TEMPLATE" \
   --stack-name "$STACK" \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides "file://./${STACK}.json"
+  --parameter-overrides "$PARAMETERS" \
+  --capabilities CAPABILITY_NAMED_IAM
